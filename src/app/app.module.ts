72cb4e01285ee60af39f09b78672b0856bb4fa8d
Luv Thakar <luv@bigscal.com>
2024-01-26 12:05:08 +0530

    signin and header navbar component created and bootstrap installed - commit1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderNavbarComponent } from './navigations/header-navbar/header-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderNavbarComponent
   ],
   imports: [
     BrowserModule,
